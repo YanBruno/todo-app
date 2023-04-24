9e2dbb83c49e5f5369d4bb9d2eee43f19ac59e74
Yan Santos <yanbrunosilvasantos@gmail.com>
2023-04-24 10:22:18 -0300

    .

---
+++ TodoApp.Test/ValueObjectTests/EmailTests.cs
@@ -0,0 +1,19 @@
+﻿using TodoApp.Core.Src.ValueObjects;
+
+namespace TodoApp.Test.ValueObjectTests;
+
+[TestClass]
+[TestCategory("ValueObject")]
+public class EmailTests
+{
+    [TestMethod]
+    public void Deve_retornar_0_notifications_dado_email_valido() {
+        Assert.AreEqual(0, new Email("yanbrunosilvasantos@gmail.com").Notifications.Count);
+    }
+
+    [TestMethod]
+    public void Deve_retornar_1_notifications_dado_email_invalido()
+    {
+        Assert.AreEqual(1, new Email("yanbrunosilvasantos@gmail").Notifications.Count);
+    }
+}
